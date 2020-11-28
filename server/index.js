73758adc6092b97e2381d6a7bc62e42ba451a7b0
Leslie Zhou <lesliezhou@Leslies-MacBook-Air.local>
2020-11-27 17:25:06 -0800

    comment on things to implement

--- server/index.js
+++ server/index.js
@@ -9,17 +9,29 @@ const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
 
 app.post('/signup', (req, res) => {
+    // fetch username from user
 
+    // no result -> insert to user and return success
+
+    // yes result -> return fail
 })
 
 app.post('/login', (req, res) => {
+    // fetch password from user
 
+    // password matches -> return success
+
+    // password not match -> return fail
 })
 
 app.post('/loglocation', (req, res) => {
-
+    // insert username, location to location
 })
 
 app.post('/report', (req, res) => {
+    // fetch all location of user from location
 
+    // for each location, fetch username from location
+
+    // send out notification to each user
 })
